48631cb33d1bb9184d5082bb741fd3cf60a053c7
LaraBerenguer <laraberenguer@outlook.com>
2025-02-19 10:51:37 +0100

    Add testing folder and testing config with Vitest.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()]
+  }),
+  defineVitestConfig({
+    test: {
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      globals: true
+    }
+  })
+);
