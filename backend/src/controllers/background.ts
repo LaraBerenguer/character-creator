a02261fccc40b9c566057de5b122f1b5541e61b5
LaraBerenguer <laraberenguer@outlook.com>
2025-02-26 18:05:14 +0100

    Initialize backend server and add background controller with placeholder methods

---
+++ backend/src/controllers/background.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const getBackgrounds = async (req: Request, res: Response) => {
+
+};
+
+export const getBackgroundsByType = async (req: Request, res: Response) => {
+
+};
+
+export const createBackground = async (req: Request, res: Response) => {
+
+};
---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+
+class Server {
+    private readonly app: express.Application;
+    private readonly port: string;
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || "3001";
+        //this.middlewares();
+        //this.routes();        
+    };
+
+    async init() {
+        //this.middlewares();
+        //this.routes();
+        //await this.dbConnect();
+        //this.seedDatabase(); para datos de base
+        
+    };
+
+    routes() {
+        this.app.get('/', (req: Request, res: Response) => {
+            res.json({
+                msg: "Api working"
+            })
+        });        
+    }
+};
