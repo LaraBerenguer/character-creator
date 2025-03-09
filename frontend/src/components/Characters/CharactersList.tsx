53538c9874963bcbb4542ec5d2af787628a36571
LaraBerenguer <laraberenguer@outlook.com>
2025-03-09 12:19:47 +0100

    Implement confirmation modal for character deletion.

--- frontend/src/components/Characters/CharactersList.tsx
+++ frontend/src/components/Characters/CharactersList.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useCharacterContext } from "../../context/CharacterContext";
 import Loading from "../../components/Loading/Loading";
 
 const charactersList = () => {
     const { removeCharacter, characters, getUserCharacters } = useCharacterContext();
     const [loading, setLoading] = useState<boolean>(false);
+    const [characterDeletingId, setCharacterDeletingId] = useState<number | null>(null);
+    const modalRef = useRef<HTMLDialogElement>(null);
+
 
     useEffect(() => {
         const fetchCharaters = async () => {
@@ -21,43 +24,72 @@ const charactersList = () => {
 
     }, []);
 
-    const handleDelete = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
-        e.preventDefault();
-        removeCharacter(id);
+    const handleConfirmDelete = () => {
+        if (characterDeletingId) {
+            removeCharacter(characterDeletingId)
+            modalRef.current?.close();
+            setCharacterDeletingId(null);
+        }
     };
+
+    const handleCancelDelete = () => {
+        modalRef.current?.close();
+        setCharacterDeletingId(null);
+    };
+
+    const handleDeleteClick = (characterId: number) => {
+        setCharacterDeletingId(characterId);
+        modalRef.current?.showModal();
+    };
+
 
     if (loading) return <Loading />;
 
     return (
-        <div className="user-dashboard-elements">
-            <div className="user-dashboard-main flex justify-center">
-                <ul className="list flex flex-col gap-5 max-w-md">
-                    <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Your characters</li>
-                    {characters.map(character => (
-                        <li key={character.id} className="list-row bg-base-100 rounded-box shadow-md p-4">
-                            <div className="list-character-info flex flex-row items-center">
-                                <div className="list-text text-start flex flex-col gap-5">
-                                    <div>
-                                        <div className="character-name text-xs uppercase font-semibold opacity-60">{character.name}</div>
+        <>
+            <div className="user-dashboard-elements">
+                <div className="user-dashboard-main flex justify-center">
+                    <ul className="list flex flex-col gap-5 max-w-md">
+                        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Your characters</li>
+                        {characters.map(character => (
+                            <li key={character.id} className="list-row bg-base-100 rounded-box shadow-md p-4">
+                                <div className="list-character-info flex flex-row items-center">
+                                    <div className="list-text text-start flex flex-col gap-5">
+                                        <div>
+                                            <div className="character-name text-xs uppercase font-semibold opacity-60">{character.name}</div>
+                                        </div>
+                                        <p className="character-description list-col-wrap text-xs">
+                                            {character.description}
+                                        </p>
                                     </div>
-                                    <p className="character-description list-col-wrap text-xs">
-                                        {character.description}
-                                    </p>
+                                    <button className="btn btn-square btn-ghost">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                    </button>
+                                    <button className="btn btn-square btn-ghost" onClick={() => { handleDeleteClick(character.id!) }}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
                                 </div>
-                                <button className="btn btn-square btn-ghost">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                </button>
-                                <button className="btn btn-square btn-ghost" onClick={(e) => { handleDelete(e, character.id!) }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+            <div>
+                <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">There's no coming back...</h3>
+                        <p className="py-4">Are you sure you want to delete this character?</p>
+                        <div className="modal-action">
+                            <button className="btn btn-error" onClick={handleConfirmDelete}>Delete</button>
+                            <button className="btn" onClick={handleCancelDelete}>Cancel</button>
+                        </div>
+                    </div>
+                </dialog>
+            </div>
+        </>
+
     );
 };
 
