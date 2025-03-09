105f508f3d27e61122c14b1022c6c0e2c6691acc
LaraBerenguer <laraberenguer@outlook.com>
2025-03-09 11:48:20 +0100

    Enhance Navbar with dropdown functionality and click outside detection for improved user experience

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,33 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
     const { user, logout } = useAuth();
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                closeDropdown();
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const closeDropdown = () => {
+        setIsOpen(false);
+    };
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
             <div className="navbar bg-base-100">
@@ -10,30 +35,35 @@ const Navbar = () => {
                     <Link className="btn btn-ghost text-xl" to='/'>Menu</Link>
                 </div>
                 <div className="flex-none gap-2">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    <div ref={dropdownRef} className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" onClick={toggleDropdown} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img
                                     alt="My Profile"
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                    src={user ? "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" : "https://images.pexels.com/photos/6985184/pexels-photo-6985184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"} />
                             </div>
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {user ?
-                                (<>
-                                    <div className="text-center mb-2"><p>Hi, {user.username}</p></div>
-                                    <li><Link to='/dashboard'>Profile</Link></li>
-                                    <li><button onClick={logout}>Logout</button></li>
-                                </>
-                                ) : (
-                                    <>
-                                        <li><Link to='/login'>Login</Link></li>
-                                        <li><Link to='/signup'>Sign Up</Link></li>
+                        {isOpen && (
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                {user ?
+                                    (<>
+                                        <div className="text-center mb-2"><p>Hi, {user.username}</p></div>
+                                        <li><Link onClick={closeDropdown} to='/dashboard'>Profile</Link></li>
+                                        <li><button onClick={() => { logout(); closeDropdown(); }}>Logout</button></li>
                                     </>
-                                )}
-                        </ul>
+                                    ) : (
+                                        <>
+                                            <li><Link onClick={closeDropdown} to='/login'>Login</Link></li>
+                                            <li><Link onClick={closeDropdown} to='/signup'>Sign Up</Link></li>
+                                        </>
+                                    )}
+                            </ul>
+                        )
+
+                        }
+
                     </div>
                 </div>
             </div>
