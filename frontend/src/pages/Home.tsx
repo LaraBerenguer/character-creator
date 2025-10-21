dc5edc7bc31fb31f4ac481093a6821413780e711
LaraBerenguer <laraberenguer@outlook.com>
2025-10-21 15:22:18 +0200

    Revise homepage content for character personality generator introduction

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,16 +7,17 @@ const Home = () => {
         <div className="flex justify-center items-center h-full mt-16 lg:mt-40">
             <div className="home-section-container flex flex-col lg:flex-row lg:justify-around items-center w-full max-w-5xl px-4">
                 <section className="text-center prose w-80 max-w-full">
-                    <h1>Welcome to your story!</h1>
+                    <h1>Who will <span className="italic text-accent/70">you</span> become?</h1>
 
-                    <p>Are you brave? Fearless? Or are you impulsive and selfless? Maybe evil?</p>
-                    <p>Find out who you will roleplay next whith this character personality generator.</p>
+                    <p>Before you face dragons or dive into danger, you must know yourself.</p>
+                    <p>Discover the heart, mind, and spirit of your next great character whith this <span className="text-accent">character personality generator</span>.</p>
                 </section>
                 <section className="text-center prose w-80 max-w-full mt-16 lg:mt-0">
                     <div>
-                        <div><Link className="no-underline hover:text-accent transition-all duration-400" to={user ? "/dashboard" : "/login"}><h2 className="text-[170%] hover:text-accent">{user ? "My Characters" : "Login"}</h2></Link></div>
+                        <div className="flex justify-end items-baseline h-full text-xs text-accent italic animate-[bounce_2s_ease-in-out_infinite]">↓↓ New! Try it now and log in at the end</div>
+                        <div><Link to="/name" className="no-underline hover:text-accent"><h2 className="text-[170%] mt-[9%] hover:text-accent transition-all duration-600">Create a new Character</h2></Link></div>
                         <div className="divider text-accent">OR</div>
-                        <div><Link to="/name" className="no-underline hover:text-accent transition-all duration-400"><h2 className="text-[170%] mt-[9%] hover:text-accent">Create a new Character</h2></Link></div>
+                        <div><Link className="no-underline hover:text-accent" to={user ? "/dashboard" : "/login"}><h2 className="text-[170%] hover:text-accent transition-all duration-600">{user ? "My Characters" : "Login"}</h2></Link></div>
                     </div>
                 </section>
             </div>
